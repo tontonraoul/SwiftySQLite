44128039c6c29bb06b8e6b15c70767d7c48e0afe
Yann Chevalier <yann.chevalier@photobox.com>
2019-11-10 20:18:09 +0100

    Initial Commit

---
+++ SwiftySQLite/SwiftySQLite.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySQLite.h
+//  SwiftySQLite
+//
+//  Created by Yann Chevalier on 10/11/2019.
+//  Copyright © 2019 BaaO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftySQLite.
+FOUNDATION_EXPORT double SwiftySQLiteVersionNumber;
+
+//! Project version string for SwiftySQLite.
+FOUNDATION_EXPORT const unsigned char SwiftySQLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySQLite/PublicHeader.h>
+
+
